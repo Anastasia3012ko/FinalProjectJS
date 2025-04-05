b884b18da43aabb1fa2a0805a0c14477c8883636
AnastasiK30 <anastasia301284@gmail.com>
2025-04-05 19:43:30 +0200

    add styles for footer section for desctop view

--- script/scriptPageOne.js
+++ script/scriptPageOne.js
@@ -1,5 +1,4 @@
 const toSecondPage = document.querySelector('#toSecondPage')
 toSecondPage.addEventListener('click', ()=>{
-   // window.location.pathname = './index2.html'
-   window.open('./index2.html', '_blank')
+   window.open('./indexTwo.html', '_blank')
 })
